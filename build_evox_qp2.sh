5619c0133bfd915d3b4c1069a853a1eaf1b818ff
rsuntk <rissu.ntk@gmail.com>
2026-01-29 11:45:45 +0700

    rename
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- build_evox_qp2.sh
+++ build_evox_qp2.sh
@@ -20,18 +20,15 @@ vendor/asus/sdm660-common
 vendor/asus/X01BD
 )
 
-echo "-- Removing ${remove_lists[@]}"
 rm -rf "${remove_lists[@]}"
 
 # init repo
-echo "-- Initializing repo"
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/rsuplaygrnd/cr_manifest.git -b 16.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Evolution-X/manifest.git -b bq2 -g default,-mips,-darwin,-notdefault
 
 # clone local manifests
-git clone https://github.com/rsuplaygrnd/local_manifest.git --depth 1 -b lineage-23.2 .repo/local_manifests
+git clone https://github.com/rsuplaygrnd/local_manifest.git --depth 1 -b lineage-23.1 .repo/local_manifests
 
 # repo sync
-echo "-- Start syncing"
 [ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
 
 # setup KernelSU
@@ -50,6 +47,6 @@ source build/envsetup.sh
 # Build the ROM
 lunch lineage_X01BD-bp4a-userdebug
 make installclean
-mka bacon
+m evolution
 
 [ -d out ] && ls out/target/product/X01BD
