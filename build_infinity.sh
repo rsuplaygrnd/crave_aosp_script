524b1e75facaadf08df9be2abdb88020647998a6
rsuntk <rissu.ntk@gmail.com>
2026-02-09 21:33:50 +0700

    build_infinity.sh: Update to build A16-QPR2
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- build_infinity.sh
+++ build_infinity.sh
@@ -1,26 +1,45 @@
 # cleanup
 remove_lists=(
     .repo/local_manifests
-    prebuilts/clang/host/linux-x86
+    device/asus/sdm660-common
+    device/asus/X01BD
+    device/lineage/sepolicy
     device/qcom/sepolicy
     device/qcom/sepolicy-legacy-um
     device/qcom/sepolicy_vndr/legacy-um
-    device/asus/sdm660-common
-    device/asus/X01BD
+    external/chromium-webview
     kernel/asus/sdm660
     out/target/product/X01BD
-    external/chromium-webview
+    prebuilts/clang/host/linux-x86
+    packages/modules/Nfc
+    packages/apps/Nfc
+    system/nfc
+    vendor/extras
+    vendor/addons
+    vendor/asus/sdm660-common
+    vendor/asus/X01BD
+    vendor/lineage-priv/keys
+    vendor/evolution-priv/keys
 )
 
+do_reclone() {
+    rm -rf $3
+    echo "-- Recloning $3 ..."
+    git clone --depth=1 $1 -b $2 $3
+}
+
+echo "-- Removing ${remove_lists[@]}"
 rm -rf "${remove_lists[@]}"
 
 # init repo
+echo "-- Initializing repo directory"
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest.git -b 16 -g default,-mips,-darwin,-notdefault
 
 # clone local manifests
-git clone https://github.com/rsuplaygrnd/local_manifest.git --depth 1 -b X01BD-16.0_Infinity_X .repo/local_manifests
+git clone https://github.com/rsuplaygrnd/local_manifest.git --depth 1 -b infinity_qpr2 .repo/local_manifests
 
 # repo sync
+echo "-- Starting to sync"
 [ -f /usr/bin/resync ] && /usr/bin/resync || /opt/crave/resync.sh
 
 # setup KernelSU
@@ -29,6 +48,15 @@ if [ -d kernel/asus/sdm660 ]; then
     curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
     cd ../../..
 fi
+
+# Setup our device/lineage/sepolicy fork
+#do_reclone https://github.com/rsuplaygrnd/device_evolution_sepolicy.git bq2 device/lineage/sepolicy
+
+# Setup our signing key, overriding existing signing key (yukiprjkt)
+rm -rf vendor/lineage-priv/keys
+git clone -b infinity https://gitlab.com/rsuntk-asus-sdm660/signing_template.git vendor/lineage-priv/keys
+cd vendor/lineage-priv/keys && bash $(pwd)/keys.sh
+cd ../../..
 
 # Set up build environment
 export BUILD_USERNAME=rsuntk
